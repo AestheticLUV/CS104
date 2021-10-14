7b7f66c0f933bcc445b27c71a315cf8b8f40d5dc
AestheticLUV <90516894+AestheticLUV@users.noreply.github.com>
2021-10-13 23:50:31 -0400

    Input and Conditions program

---
+++ conditions.py
@@ -0,0 +1,18 @@
+temp = 0
+
+temp = int(input('Please Enter the current temperature: '))
+
+if temp >= 90:
+    print ('Wear Shorts')
+
+elif temp >= 70:
+    print ('Short Sleeves are fine')
+
+elif temp >= 50:
+    print ('Wear A Jacket')
+
+elif temp >= 32:
+    print ('Wear a heavy Coat')
+
+elif temp < 32:
+    print ('Stay Inside')
